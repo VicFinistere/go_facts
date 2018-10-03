40dd5fa64d0a0abd119c1b5ea5c84fa9c6276a70
Zoeinfp <zoebelleton@gmail.com>
2018-10-03 10:57:35 +0200

    encode before

--- store/logic.py
+++ store/logic.py
@@ -414,11 +414,12 @@ def url_category_for_grade(category, grade):
     :return: url
     """
 
+    category = category.encode('utf-8')
     url = "https://fr.openfoodfacts.org/cgi/search.pl?action=process" \
           "&tagtype_0=categories&tag_contains_0=contains&tag_0={}" \
           "&tagtype_1=nutrition_grades&tag_contains_1=contains&tag_1={}" \
           "&sort_by=unique_scans_n&page_size=20&axis_x=energy&axis_y=products_n" \
-          "&action=display".format(category.encode('utf8'), grade)
+          "&action=display".format(category, grade)
 
     try:
         return url
