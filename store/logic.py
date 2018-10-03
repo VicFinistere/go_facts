407f69a7650720e24d0526e883b758d75934cdd6
Zoeinfp <zoebelleton@gmail.com>
2018-10-03 10:18:44 +0200

    rewriting url

--- store/logic.py
+++ store/logic.py
@@ -413,7 +413,7 @@ def url_category_for_grade(category, grade):
     :param grade:
     :return: url
     """
-
+    
     return "https://fr.openfoodfacts.org/cgi/search.pl?action=process&" \
            "tagtype_0=categories&tag_contains_0=contains&tag_0={}" \
            "&tagtype_1=nutrition_grades&tag_contains_1=contains&tag_1={}" \
