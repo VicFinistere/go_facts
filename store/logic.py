831f405d8acdb41c45c5f487623fff801fbaf636
Zoeinfp <zoebelleton@gmail.com>
2018-10-03 13:06:00 +0200

    request encoded with base64 url

--- store/logic.py
+++ store/logic.py
@@ -86,7 +86,7 @@ def fetch_products_id(url):
     :param url: Page
     :return: products_id
     """
-    data = requests.get(url.encode('utf-8'))
+    data = requests.get(base64.b64encode(url))
     results = data.text
     products_id = re.findall(r'<a href="/produit/(\d+)/', results)
     return products_id
