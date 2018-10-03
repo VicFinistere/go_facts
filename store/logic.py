06a41dc639764d5b26f0ef0dd3697dceef6fb792
Zoeinfp <zoebelleton@gmail.com>
2018-10-03 10:29:22 +0200

    separate url from return instruction

--- store/logic.py
+++ store/logic.py
@@ -414,12 +414,14 @@ def url_category_for_grade(category, grade):
     :return: url
     """
 
+    url = "https://fr.openfoodfacts.org/cgi/search.pl?action=process&" \
+          "tagtype_0=categories&tag_contains_0=contains&tag_0={}" \
+          "&tagtype_1=nutrition_grades&tag_contains_1=contains&tag_1={}" \
+          "&sort_by=unique_scans_n&page_size=20&axis_x=energy&axis_y=products_n" \
+          "&action=display".format(category, grade)
+
     try:
-        return "https://fr.openfoodfacts.org/cgi/search.pl?action=process&" \
-               "tagtype_0=categories&tag_contains_0=contains&tag_0={}" \
-               "&tagtype_1=nutrition_grades&tag_contains_1=contains&tag_1={}" \
-               "&sort_by=unique_scans_n&page_size=20&axis_x=energy&axis_y=products_n" \
-               "&action=display".format(category, grade)
+        return url
     except ValueError:
         return None
 
