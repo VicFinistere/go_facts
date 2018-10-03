a798ec48e5d03fefccd58ccbc33c8fb7f646704f
Zoeinfp <zoebelleton@gmail.com>
2018-10-03 10:36:52 +0200

    try category and grade

--- store/logic.py
+++ store/logic.py
@@ -413,7 +413,8 @@ def url_category_for_grade(category, grade):
     :param grade:
     :return: url
     """
-
+    logging.info(category)
+    logging.info(grade)
     url = "https://fr.openfoodfacts.org/cgi/search.pl?action=process" \
           "&tagtype_0=categories&tag_contains_0=contains&tag_0={}" \
           "&tagtype_1=nutrition_grades&tag_contains_1=contains&tag_1={}" \
